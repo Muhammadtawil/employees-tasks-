a5c8299c679bc3b7b23709e2b70172e0e3012169
Muhammad Tawil <muhammad.tawil@hotmail.com>
2022-10-25 23:25:22 +0300

    add splash screen

---
+++ employeestasks/lib/utilis/pathimage.dart
@@ -0,0 +1,10 @@
+class PathImage {
+  static const String background1 = "assets/images/Background1.png";
+  static const String background2 = "assets/images/Background2.png";
+  static const String background3 = "assets/images/Background3.png";
+  static const String logo = "assets/images/Logo.png";
+  static const String arbic = "assets/images/English.png";
+  static const String urdu = "assets/images/Urdu.png";
+  static const String english = "assets/images/Arbic.png";
+  static const String bGEmdadat = "assets/images/bgemdadat.png";
+}
